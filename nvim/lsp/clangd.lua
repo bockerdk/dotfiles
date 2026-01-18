892f27789b1fe37f990ff7cd672e3513cb95239d
bockerdk <public@bocker.dk>
2026-01-18 09:23:07 +0100

    Nvim lsp clangd add tidy param

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -1,5 +1,5 @@
 return {
-    cmd = { 'clangd', '--background-index' },
+    cmd = { 'clangd', '--background-index', '--clang-tidy' },
     root_markers = { 'compile_commands.json' },
     filetypes = { 'c', 'cpp' },
 }
